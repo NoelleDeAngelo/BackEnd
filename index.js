c020e1b323c249d528fcad76477b26ce48703a49
Sidney <sid.nguyen@gmail.com>
2021-05-25 21:05:43 -0700

    socket emits number of listener
    Co-Authored-By: Noelle DeAngelo <github: Noelle DeAngelo>

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const bodyParser = require('body-parser')
-const controller = require('./controllers')
-const multer = require('multer')
-const storage = multer.memoryStorage()
-var form = multer({dest: 'form/', storage: storage})
-const {v4: uuidV4} = require('uuid');
+const bodyParser = require('body-parser');
+const controller = require('./controllers');
+const multer = require('multer');
+const storage = multer.memoryStorage();
+var form = multer({dest: 'form/', storage: storage});
 const io =require('socket.io')(http, {
   cors: {
     origin: "http://localhost:8100",
@@ -23,21 +22,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/addAudio', form.single('audio'), controller.s3);
 
 
-
-// app.get('/stream', (req, res)=>{
-//   res.render('index.ejs', {roomId: req.params.room})
-// })
+var numClients = {};
 
 io.on('connection', (socket) => {
   socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).emit('user-connected', userId)
+    socket.join(roomId);
+    if (numClients[roomId] === undefined) {
+      numClients[roomId] = 1;
+    } else {
+      numClients[roomId]++;
+    }
+    io.in(roomId).emit('number-users', numClients[roomId]);
+    socket.in(roomId).emit('user-connected', userId);
+
     socket.on('disconnect', ()=> {
-      socket.to(roomId).emit('user-disconnect', userId)
+        socket.in(roomId).emit('user-disconnect', userId);
+      numClients[roomId]--;
+      io.in(roomId).emit('number-users', numClients[roomId]);
+    })
+    socket.on('rendered',  ()=>{
+      io.in(roomId).emit('number-users', numClients[roomId]);
     })
   })
 })
 
 http.listen(4000, () => {
-  console.log('Socket listening on 4000')
+  console.log('Socket listening on 4000');
 })
