19b733b84929a7a9026aa1f146032ec57e332312
Sidney <sid.nguyen@gmail.com>
2021-05-26 17:04:21 -0700

    removed logs and comments

--- controllers/audioS3.js
+++ controllers/audioS3.js
@@ -13,10 +13,8 @@ const s3 = new aws.S3()
 const S3_BUCKET = process.env.awsBucket_parley;
 
 module.exports = async (req, res) => {
-  console.log(req.file, 'Test')
   const fileName = req.file.originalname;
   const fileType = req.file.mimetype
-  // const test = 'test'
   const s3Params = {
     Bucket: S3_BUCKET,
     Key: fileName,
@@ -33,7 +31,6 @@ module.exports = async (req, res) => {
       }
     })
     const qaImgUrl = `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
-    console.log(qaImgUrl)
     res.send(qaImgUrl)
   } catch (err) {
     res.send(err)
